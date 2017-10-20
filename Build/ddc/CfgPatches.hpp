93ca691390049bbc47fad6e05db44768802932f2
Schwerer_Konigstiger <karan.vir.singh.kvs@gmail.com>
2017-10-20 11:41:32 -0400

    WORK GODDAMNIT
    Fixed DDC.pbo
    Fixed Music.pbo
    Fixed fucking pointers
    Fuck pointers

---
+++ Build/ddc/CfgPatches.hpp
@@ -0,0 +1,8 @@
+class CfgPatches {
+	class DDC_Core {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"A3_Weapons_F", "A3_Characters_F_BLUFOR"};
+	};
+};
--- Build/music/CfgMusicClasses.hpp
+++ Build/music/CfgMusicClasses.hpp
@@ -3,154 +3,154 @@ class CfgMusicClasses
 	class ddc_music_america_fuck_yea
 	{
 		name = "[DDC]America Fuck Yea"; //song display name
-		sound[] = {"ddc\data\americafuckyea.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\americafuckyea.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_american_march
 	{
 		name = "[DDC]American March"; //song display name
-		sound[] = {"ddc\data\americanmarch.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\americanmarch.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_back_in_black
 	{
 		name = "[DDC]Back in Black"; //song display name
-		sound[] = {"ddc\data\backinblack.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\backinblack.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_british_grenadiers
 	{
 		name = "[DDC]British Grenadiers"; //song display name
-		sound[] = {"ddc\data\britishgrenadiers.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\britishgrenadiers.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_erika
 	{
 		name = "[DDC]Erika"; //song display name
-		sound[] = {"ddc\data\erika.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\erika.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_fortunate_son
 	{
 		name = "[DDC]Fortunate Son"; //song display name
-		sound[] = {"ddc\data\fortunateson.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\fortunateson.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_going_in_for_guns
 	{
 		name = "[DDC]Going in for Guns"; //song display name
-		sound[] = {"ddc\data\goinginforguns.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\goinginforguns.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_highway_to_hell
 	{
 		name = "[DDC]Highway to Hell"; //song display name
-		sound[] = {"ddc\data\highwaytohell.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\highwaytohell.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_aces_high
 	{
 		name = "[DDC]Aces high"; //song display name
-		sound[] = {"ddc\data\aceshigh.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\aceshigh.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_iron_man
 	{
 		name = "[DDC]Iron Man"; //song display name
-		sound[] = {"ddc\data\ironman.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\ironman.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_a_10
 	{
 		name = "[DDC]I wish I had a Gun Just Like the A-10"; //song display name
-		sound[] = {"ddc\data\a10.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\a10.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_kachusya
 	{
 		name = "[DDC]Kachusya"; //song display name
-		sound[] = {"ddc\data\Kachusya.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\Kachusya.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_number_beast
 	{
 		name = "[DDC]Number of the Beast"; //song display name
-		sound[] = {"ddc\data\numberofthebeast.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\numberofthebeast.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_panzer_battalion
 	{
 		name = "[DDC]Panzer Battalion"; //song display name
-		sound[] = {"ddc\data\panzerbattalion.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\panzerbattalion.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_panzerlied
 	{
 		name = "[DDC]Panzerlied"; //song display name
-		sound[] = {"ddc\data\panzerlied.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\panzerlied.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_paradise_city
 	{
 		name = "[DDC]Paradise City"; //song display name
-		sound[] = {"ddc\data\paradisecity.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\paradisecity.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_shoot_to_thrill
 	{
 		name = "[DDC] Shoot to Thrill"; //song display name
-		sound[] = {"ddc\data\shoottothrill.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\shoottothrill.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_soviet_march
 	{
 		name = "[DDC]Soviet March"; //song display name
-		sound[] = {"ddc\data\sovietmarch.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\sovietmarch.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_tank
 	{
 		name = "[DDC]Tank!"; //song display name
-		sound[] = {"ddc\data\tank.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\tank.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_thunderstruck
 	{
 		name = "[DDC]Thunderstruck"; //song display name
-		sound[] = {"ddc\data\thunderstruck.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\thunderstruck.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_war_pigs
 	{
 		name = "[DDC]War Pigs"; //song display name
-		sound[] = {"ddc\data\warpigs.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\warpigs.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
 	class ddc_music_welcome_jungle
 	{
 		name = "[DDC]Welcome to the Jungle"; //song display name
-		sound[] = {"ddc\data\welcometothejungle.ogg",db+0,1}; //ogg file +Db
+		sound[] = {"music\data\welcometothejungle.ogg",db+0,1}; //ogg file +Db
 		duration=200; //in seconds
 		musicClass = "ddc_music"; //always ddc_music in this case
 	};
