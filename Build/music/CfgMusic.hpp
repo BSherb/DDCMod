1f1475c86bc7cac34812466ecdd2331e8271f325
Schwerer_Konigstiger <karan.vir.singh.kvs@gmail.com>
2017-10-29 20:45:11 -0400

    Music Massive Fixes
    Swapped CfgMusic & CfgMusicClasses
    Added Time in Seconds
    Added Type

--- Build/music/CfgMusic.hpp
+++ Build/music/CfgMusic.hpp
@@ -1,14 +1,179 @@
 class CfgMusic
 {
-	tracks[]+=
+	class gsc_music_america_fuck_yea
 	{
-		"songdisplayname",
+		name = "[DDC]America Fuck Yea"; //song display name
+		sound[] = {"music\data\americafuckyea.ogg",db+0,1}; //ogg file +Db
+		duration=163; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
 	};
-
-
-
-
-
-
-	#include "CfgMusicClasses.hpp"
+	class gsc_music_american_march
+	{
+		name = "[DDC]American March"; //song display name
+		sound[] = {"music\data\americanmarch.ogg",db+0,1}; //ogg file +Db
+		duration=137; //in seconds
+		musicClass = "GSC Music"; //always GSC Music in this case
+		theme = "Score";
+	};
+	class gsc_music_back_in_black
+	{
+		name = "[DDC]Back in Black"; //song display name
+		sound[] = {"music\data\backinblack.ogg",db+0,1}; //ogg file +Db
+		duration=257; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_british_grenadiers
+	{
+		name = "[DDC]British Grenadiers"; //song display name
+		sound[] = {"music\data\britishgrenadiers.ogg",db+0,1}; //ogg file +Db
+		duration=97; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_erika
+	{
+		name = "[DDC]Erika"; //song display name
+		sound[] = {"music\data\erika.ogg",db+0,1}; //ogg file +Db
+		duration=94; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_fortunate_son
+	{
+		name = "[DDC]Fortunate Son"; //song display name
+		sound[] = {"music\data\fortunateson.ogg",db+0,1}; //ogg file +Db
+		duration=138; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_going_in_for_guns
+	{
+		name = "[DDC]Going in for Guns"; //song display name
+		sound[] = {"music\data\goinginforguns.ogg",db+0,1}; //ogg file +Db
+		duration=200; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_highway_to_hell
+	{
+		name = "[DDC]Highway to Hell"; //song display name
+		sound[] = {"music\data\highwaytohell.ogg",db+0,1}; //ogg file +Db
+		duration=210; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_aces_high
+	{
+		name = "[DDC]Aces high"; //song display name
+		sound[] = {"music\data\aceshigh.ogg",db+0,1}; //ogg file +Db
+		duration=271; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_iron_man
+	{
+		name = "[DDC]Iron Man"; //song display name
+		sound[] = {"music\data\ironman.ogg",db+0,1}; //ogg file +Db
+		duration=358; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_a_10
+	{
+		name = "[DDC]I wish I had a Gun Just Like the A-10"; //song display name
+		sound[] = {"music\data\a10.ogg",db+0,1}; //ogg file +Db
+		duration=126; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_kachusya
+	{
+		name = "[DDC]Kachusya"; //song display name
+		sound[] = {"music\data\Kachusya.ogg",db+0,1}; //ogg file +Db
+		duration=92; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_number_beast
+	{
+		name = "[DDC]Number of the Beast"; //song display name
+		sound[] = {"music\data\numberofthebeast.ogg",db+0,1}; //ogg file +Db
+		duration=351; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_panzer_battalion
+	{
+		name = "[DDC]Panzer Battalion"; //song display name
+		sound[] = {"music\data\panzerbattalion.ogg",db+0,1}; //ogg file +Db
+		duration=307; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_panzerlied
+	{
+		name = "[DDC]Panzerlied"; //song display name
+		sound[] = {"music\data\panzerlied.ogg",db+0,1}; //ogg file +Db
+		duration=148; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_paradise_city
+	{
+		name = "[DDC]Paradise City"; //song display name
+		sound[] = {"music\data\paradisecity.ogg",db+0,1}; //ogg file +Db
+		duration=404; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_shoot_to_thrill
+	{
+		name = "[DDC] Shoot to Thrill"; //song display name
+		sound[] = {"music\data\shoottothrill.ogg",db+0,1}; //ogg file +Db
+		duration=317; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_soviet_march
+	{
+		name = "[DDC]Soviet March"; //song display name
+		sound[] = {"music\data\sovietmarch.ogg",db+0,1}; //ogg file +Db
+		duration=279; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_tank
+	{
+		name = "[DDC]Tank!"; //song display name
+		sound[] = {"music\data\tank.ogg",db+0,1}; //ogg file +Db
+		duration=210; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_thunderstruck
+	{
+		name = "[DDC]Thunderstruck"; //song display name
+		sound[] = {"music\data\thunderstruck.ogg",db+0,1}; //ogg file +Db
+		duration=294; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_war_pigs
+	{
+		name = "[DDC]War Pigs"; //song display name
+		sound[] = {"music\data\warpigs.ogg",db+0,1}; //ogg file +Db
+		duration=474; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
+	class gsc_music_welcome_jungle
+	{
+		name = "[DDC]Welcome to the Jungle"; //song display name
+		sound[] = {"music\data\welcometothejungle.ogg",db+0,1}; //ogg file +Db
+		duration=273; //in seconds
+		musicClass = "GSC Music"; //always gsc_music in this case
+		theme = "Score";
+	};
 };
